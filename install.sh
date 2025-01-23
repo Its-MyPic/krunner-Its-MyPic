482165da7243f9a4cc523516e82536a5b6907365
Jeff <jeffpeng2012@gmail.com>
2025-01-23 20:38:54 +0800

    chore: update install script

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# Standalone install script for copying files
-
 set -e
 
 prefix="${XDG_DATA_HOME:-$HOME/.local/share}"
 krunner_dbusdir="$prefix/krunner/dbusplugins"
 services_dir="$prefix/dbus-1/services/"
 
+plugin_dir="$HOME/.local/share/its_mypic"
+
+mkdir -p $plugin_dir
 mkdir -p $krunner_dbusdir
 mkdir -p $services_dir
 
 cp its_mypic.desktop $krunner_dbusdir
-printf "[D-BUS Service]\nName=org.kde.its_mypic\nExec=\"$PWD/main.py\"" > $services_dir/org.kde.its_mypic.service
+cp -r data main.py database.py clipper $plugin_dir
+printf "[D-BUS Service]\nName=org.kde.its_mypic\nExec=\"$plugin_dir/main.py\"" > $services_dir/org.kde.its_mypic.service
 
 kquitapp6 krunner
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Exit if something fails
 set -e
 
 prefix="${XDG_DATA_HOME:-$HOME/.local/share}"
 krunner_dbusdir="$prefix/krunner/dbusplugins"
+plugin_dir="$HOME/.local/share/its_mypic"
 
+
+rm -r $plugin_dir
 rm $prefix/dbus-1/services/org.kde.its_mypic.service
 rm $krunner_dbusdir/its_mypic.desktop
 kquitapp6 krunner
